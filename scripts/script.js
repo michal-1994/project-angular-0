bb42a3d7d659045e48b2a987ea48da8d91246734
Michal <michalgrzegorczyk04@gmail.com>
2021-07-04 11:52:57 +0200

    Update project

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function Cart($scope) {
-    $scope.description = 'Title product';
-    $scope.cost = 20.00;
+    $scope.description = 'Title product 1';
+    $scope.cost = 25.00;
     $scope.qty = 1;
     $scope.currency = ' zł';
-  }
+}
